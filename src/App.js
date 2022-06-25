1a2feb8bc25f745f71d6a9e2e5162dfde8239217
Johnnds <jonnhds@gmail.com>
2022-06-24 21:32:11 -0300

    Creación de vista y consumo de las APIs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Descubrir from './Descubrir';
+import { Navbar, Nav, Container, Form, FormControl, Button } from 'react-bootstrap';
 
-function App() {
+
+const API_Search = 'https://api.themoviedb.org/3/search/movie?api_key=fd3e3b49c9e6f23e1fe7bf95cfe08a89'
+
+
+const App = () => {
+
+  const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    const getMovies = async () => {
+      const response = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=fd3e3b49c9e6f23e1fe7bf95cfe08a89');
+      const discovery = await response.json();
+      console.log(discovery);
+      setMovies(discovery.results)
+    }
+
+    getMovies();
+  }, []);
+
+  const searchMovie = async (e) => {
+    e.preventDefault();
+    console.log("Searching");
+    try {
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=fd3e3b49c9e6f23e1fe7bf95cfe08a89&query=${query}`;
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+      setMovies(data.results);
+    }
+    catch (e) {
+      console.log(e);
+    }
+  }
+
+  const changeHandler = (e) => {
+    setQuery(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <Navbar bg="dark" expand="lg" variant="dark">
+        <Container fluid>
+          <Navbar.Brand href="/home">Mundo Pelis</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll"></Navbar.Toggle>
+
+          <Navbar.Collapse id="nabarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-3"
+              style={{ maxHeight: '100px' }}
+              navbarScroll></Nav>
+
+            <Form className="d-flex" onSubmit={searchMovie} autoComplete="off">
+              <FormControl
+                type="search"
+                placeholder="Buscar Pelicula"
+                className="me-2"
+                aria-label="search"
+                name="query"
+                value={query} onChange={changeHandler}></FormControl>
+              <Button variant="secondary" type="submit">Buscar</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <main className='container'>
+        <div>
+        <div><h2>PARA DESCUBRIR</h2></div>
+          {movies.length > 0 ? (
+            <div className="container">
+              <div className="grid">
+                {movies.map((movieReq) =>
+                  <Descubrir  key={movieReq.id} {...movieReq} />)}
+              </div>
+            </div>
+          ) : (
+            <h2>Ha ocurrido un error :c </h2>
+          )}
+        </div>
+
+      </main>
+    </>
   );
 }
 
 export default App;
+
